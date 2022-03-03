723bde8197cfa22c8e5b5296d93948b3f84d52b8
darlyncarmenate <darlyncarmenate@gmail.com>
2022-03-03 06:38:31 +0200

    Task_02

---
+++ Task_02/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = new Random().Next(0, 50);
+int b = new Random().Next(0, 50);
+int c = new Random().Next(0, 50);
+Console.WriteLine($"{a}, {b}, {c}");
+
+if(a > b && a > c)
+{
+    Console.WriteLine($"Максимальное число: {a}");
+}
+else if(b > c && b > a)
+{
+    Console.WriteLine($"Максимальное число: {b}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число: {c}");
+}
