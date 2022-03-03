a3edd98abf0fb151eb3a472e4e988fe4aac851e4
darlyncarmenate <darlyncarmenate@gmail.com>
2022-03-03 06:22:40 +0200

    Task_01

---
+++ Task_01/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число: ");
+string first = Console.ReadLine();
+int a = int.Parse(first);
+
+Console.WriteLine("Введите втооое число: ");
+string second = Console.ReadLine();
+int b = int.Parse(second);
+
+int max = a;
+
+if(b > max)
+{
+    max = b;
+}
+
+Console.Write($"Наибольшее число: {max}");
